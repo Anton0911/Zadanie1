f7a0702632360660b9fe5b9ee6db79f215fee913
Anton <smirnovanton123@icloud.com>
2022-12-18 16:55:07 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       int bilet = 2000;
+       int OneMily = 20;
+       int bonys = bilet / OneMily;
+
+        System.out.println(bonys);
+              }
+    }
